00aa84f76dc9fb328c5ca89c8cba67ca0658a597
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 14:34:37 +1200

    kk

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const button = document.querySelector('#button');
 button.addEventListener('click', function() {
 	navigator.bluetooth.requestDevice({
-		filters: [{
-			name : "C03"
-		}]
+		acceptAllDevices: true
 	}).then(device => {
 		console.log('Got device:', device.name);
 		console.log('id:', device.id);
