d1361f98a24a973bb3862f21e5e44a9922838b7c
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 14:24:47 +1200

    fixed js bug

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const button = document.querySelector("#button");
-
-button.addEventListener("click", async function() {
+const button = document.querySelector('#button');
+button.addEventListener('click', function() {
 	navigator.bluetooth.requestDevice({
 		filters: [{
-			name: "C03"
+			services: ['battery_service']
 		}]
 	}).then(device => {
-		console.log("Got device: ", device.name);
-		document.getElementById("output").innerHTML = "DeviceId:" + device.id;
-	})
+		console.log('Got device:', device.name);
+		console.log('id:', device.id);
+	});
+});
