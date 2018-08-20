2b6bb55238196bd57e253c9118656adbe4fd02e7
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 15:32:57 +1200

    debugging

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const button = document.querySelector('#button');
 button.addEventListener('click', function() {
 	navigator.bluetooth.requestDevice({
 		filters: [{
-			name : 'C03',
-			services: ['0000ffe1-0000-1000-8000-00805f9b34fb']
+			services: ['0000ffe0-0000-1000-8000-00805f9b34fb', '0000ffe1-0000-1000-8000-00805f9b34fb']
 	}]
 	}).then(device => {
 		console.log('Got device:', device.name);
