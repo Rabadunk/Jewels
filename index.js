0d78550976b912b49b55bf80276c81b13c80a7ee
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 14:28:42 +1200

    fixed js bug

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const button = document.querySelector('#button');
 button.addEventListener('click', function() {
 	navigator.bluetooth.requestDevice({
 		filters: [{
-			name : "C03";
+			name : "C03"
 		}]
 	}).then(device => {
 		console.log('Got device:', device.name);
