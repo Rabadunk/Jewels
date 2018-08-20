dd5e48b3163458e609926550de9f6e8929cfab87
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 16:07:35 +1200

    debugging

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ const sparky = 'C03';
 
 const button = document.querySelector('#button');
 button.addEventListener('click', function() {
-	navigator.bluetooth.requestDevice({ filters:
-		name: sparky,
-		services: [service]
-	})
+	navigator.bluetooth.requestDevice({ filters: [{ services: [service] }] })
 	    .then(device => device.gatt.connect())
 	    .then(server => server.getPrimaryService(service))
 	    .then(service => service.getCharacteristic(characteristic))
 	    .then(characteristic => {
 	        characteristicInstance = characteristic;
-					setInterval(updateSpeed,200); //send commands every 200ms
-					console.log(characteristic.readValue());
-	  			return characteristic.readValue();
+		setInterval(updateSpeed,200); //send commands every 200ms
+			console.log("reading value");
+	  	return characteristic.readValue();
 	    })
 	.catch(error => { console.log(error); });
-})
 });
