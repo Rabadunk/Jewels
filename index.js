0df5d0cb8492db60e6969a8e88614a6cef5b9eec
Jono <jalp521@aucklanduni.ac.nz>
2018-08-20 16:53:24 +1200

    debugging

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const service = 0xFFE0;
 const characteristic = 0xFFE1;
 const sparky = 'C03';
-
 const button = document.querySelector('#button');
+
 button.addEventListener('click', function() {
+
 	navigator.bluetooth.requestDevice({ filters: [{ services: [service] }] })
 	    .then(device => device.gatt.connect())
 	    .then(server => server.getPrimaryService(service))
 	    .then(service => service.getCharacteristic(characteristic))
 	    .then(characteristic => {
 	        characteristicInstance = characteristic;
-			console.log("reading value");
-	  	return characteristic.readValue();
+					characteristic.addEventListener('characteristicvaluechanged',
+					        handleValueChange);
 	    })
-	.catch(error => { console.log(error); });
+	.catch(error => { console.log("This is error: ", error); });
 });
+
+function handleValueChange(event){
+	let value = event.target.value.getUint8(0);
+	console.log('Our value is: ', value);
+}
