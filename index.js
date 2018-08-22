f08e80e00f139a7d787f3ffa53b43937e74a63bf
Jonathan Alphonso <31195359+Rabadunk@users.noreply.github.com>
2018-08-22 16:27:58 +1200

    Added comments to explain things

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+// Constants
+
+//UUID for ATMega328pb
 const deviceService = 0xFFE0;
 const deviceCharacter = 0xFFE1;
-const sparky = 'C03';
-const button = document.querySelector('#button');
 
+//Name of our HM10 bluetooth module
+const sparky = 'C03';
+
+
+// When button is clicked, start bluetooth navigator.
+const button = document.querySelector('#button');
 button.addEventListener('click', function() {
 
 	navigator.bluetooth.requestDevice({ filters: [{ services: [deviceService] }] })
+			// GATT connect.
 	    .then(device => device.gatt.connect())
 	    .then(server => server.getPrimaryService(deviceService))
 	    .then(service => service.getCharacteristic(deviceCharacter))
@@ -17,6 +25,7 @@ button.addEventListener('click', function() {
 	.catch(error => { console.log("This is error: ", error); });
 });
 
+// Function for sending data to front end.
 function handleValueChange(event){
 	var value = event.target.value.getUint8(0);
     document.getElementById("output").innerHTML = value;
